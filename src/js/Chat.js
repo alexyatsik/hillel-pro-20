16e6aa6ede727cf71042daf2fa3b8dbc522ea5f8
Alexeii Yatsenko <alexeii.yatsenko@gmail.com>
2020-04-06 23:03:05 +0300

    almost done

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -2,10 +2,12 @@
 
 class Chat {
     constructor() {
+        this.isActive = true;
         const root = $nR('#chat-app');
         root.classList.add('chat-app');
         this.chatWindow = document.createElement('div');
         this.chatWindow.classList.add('chat-app__window');
+        this.chatText = document.createElement('ul');
 
         this.userInteraction = document.createElement('div');
         this.userInteraction.classList.add('chat-app__user-interaction');
@@ -13,31 +15,67 @@ class Chat {
         this.input = document.createElement('input');
         this.input.setAttribute('type', 'text');
         this.input.setAttribute('placeholder', 'Message...');
+
         this.send = document.createElement('input');
         this.send.setAttribute('type', 'button');
         this.send.setAttribute('value', 'Send');
+        this.send.addEventListener('click', () => {
+            if (!this.input.value) {
+                this.input.value = '';
+                return;
+            }
 
+            if (this.input.value === 'My watch has ended') {
+                this.showMessage(this.input.value);
+                this.input.value = '';
+                clearInterval(this.booring);
+                this.isActive = false;
+                this.showMessage('До встречи!');
+                return;
+            }
+            
+            clearInterval(this.booring);
+            this.showMessage(this.input.value);
+            this.input.value = '';
+            if (this.isActive) {
+                this.answer();
+            }
+        });
+
+        this.chatWindow.appendChild(this.chatText);
         this.userInteraction.appendChild(this.input);
         this.userInteraction.appendChild(this.send);
         root.appendChild(this.chatWindow);
         root.appendChild(this.userInteraction);
     }
 
-    showMessage() {
-        this.chat();
+    showMessage(value) {
+        const message = document.createElement('li');
+        message.textContent = value;
+        this.chatText.appendChild(message);
     }
 
-    async randomMessage() {
+    async answers() {
         const message = [
             'Привет',
             'Куда пропал?',
-            'Давно не виделись'
-        ][rand(0, 2)];
-        return timeout(message, 5);
+            'Давно не виделись',
+            'Как жизнь?',
+            'Как семья?',
+            'Хорошо выглядишь!'
+        ][rand(0, 5)];
+        return timeout(message, rand(0, 10));
     }
     
-    async chat() {
-        const message = await this.randomMessage();
-        this.chatWindow.innerHTML = message;
+    async answer(bye = false) {
+        const message = await this.answers();
+        this.showMessage(message);
+        this.booring = setTimeout(() => {
+            if (!this.isActive) {
+                return;
+            }
+            this.showMessage(`Мне пора. Увидимся позже.`);
+            this.isActive = false;
+        }, 10000);
     }
 }
